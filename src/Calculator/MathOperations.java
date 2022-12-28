2246f373a0cb1a6732868c77fd8a5fd7ec2285fa
Jefferson <jefferson-67@live.com>
2022-12-28 19:34:08 -0300

    tdd simplified

---
+++ src/Calculator/MathOperations.java
@@ -0,0 +1,19 @@
+package Calculator;
+
+public class MathOperations {
+    public static int Sum(int a, int b){
+        return a+b;
+    }
+
+    public static int Sub(int a, int b){
+        return a-b;
+    }
+
+    public static int Multiplication(int a, int b){
+        return a*b;
+    }
+
+    public static int Division(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/CalculatorTest/MathOperationsTest.java
@@ -0,0 +1,86 @@
+package CalculatorTest;
+
+import Calculator.MathOperations;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MathOperationsTest {
+
+    @Test
+    public void SumTest(){
+        int result = MathOperations.Sum(2,2);
+        int expectedResult = 4;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void SumTest2(){
+        int result = MathOperations.Sum(-2,-2);
+        int expectedResult = -4;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void SumTest3(){
+        int result = MathOperations.Sum(2,-2);
+        int expectedResult = 0;
+        assertEquals(result, expectedResult);
+    }@Test
+    public void SumTest4(){
+        int result = MathOperations.Sum(-2,1);
+        int expectedResult = -1;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void SubTest(){
+        int result = MathOperations.Sub(2,2);
+        int expectedResult = 0;
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    public void SubTest2(){
+        int result = MathOperations.Sub(-2,2);
+        int expectedResult = -4;
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    public void SubTest3(){
+        int result = MathOperations.Sub(2,1);
+        int expectedResult = 1;
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    public void MultiplyTest(){
+        int result = MathOperations.Multiplication(2,2);
+        int expectedResult = 4;
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    public void MultiplyTest2(){
+        int result = MathOperations.Multiplication(2,5);
+        int expectedResult = 10;
+        assertEquals(result, expectedResult);
+    }
+    @Test
+    public void MultiplyTest3(){
+        int result = MathOperations.Multiplication(2,-2);
+        int expectedResult = -4;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void DivisionTest(){
+        int result = MathOperations.Division(2,2);
+        int expectedResult = 1;
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void DivisionTest2(){
+        int result = MathOperations.Division(2,1);
+        int expectedResult = 2;
+        assertEquals(result, expectedResult);
+    }
+}
